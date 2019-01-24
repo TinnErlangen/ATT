55863fc3f262d559e283c2ce48249719c4e4c928
jeff <jeff.hanna@gmail.com>
2019-01-24 15:42:11 +0100

    ?

--- doica_ref.py
+++ doica_ref.py
@@ -1,11 +1,14 @@
 import mne
 import numpy as np
 
-subjs = ["ATT_10","ATT_11","ATT_12","ATT_13","ATT_14","ATT_15","ATT_16",
-"ATT_17","ATT_18","ATT_19"]
-subjs = ["NEM_16"]
+# do an ICA decomposition on data, and reference channels.
+
+subjs = ["ATT_10", "ATT_11", "ATT_12", "ATT_13", "ATT_14", "ATT_15", "ATT_16",
+         "ATT_17", "ATT_18", "ATT_19", "ATT_20", "ATT_21", "ATT_22", "ATT_23",
+         "ATT_24", "ATT_25", "ATT_26", "ATT_27", "ATT_28"]
+#subjs = ["NEM_16"]
 runs = [str(x+1) for x in range(5)]
-runs = ["3"]
+#runs = ["3"]
 
 base_dir ="../"
 proc_dir = base_dir+"proc/"
@@ -18,8 +21,13 @@ for sub in subjs:
                                        method="picard",allow_ref_meg=True)
         picks = mne.pick_types(raw.info,meg=False,ref_meg=True)
         icaref.fit(raw,picks=picks)
-        icaref.save("{dir}nc_{sub}_{run}_hand_ref-ica.fif".format(dir=proc_dir,sub=sub,run=run))
-        icameg = mne.preprocessing.ICA(n_components=None,max_iter=1000,method="picard")
+        icaref.save("{dir}nc_{sub}_{run}_hand_ref-ica.fif".format(dir=proc_dir,
+                                                                  sub=sub,
+                                                                  run=run))
+        icameg = mne.preprocessing.ICA(n_components=None,max_iter=1000,
+                                       method="picard")
         picks = mne.pick_types(raw.info,meg=True,ref_meg=False)
         icameg.fit(raw,picks=picks)
-        icameg.save("{dir}nc_{sub}_{run}_hand_meg-ica.fif".format(dir=proc_dir,sub=sub,run=run))
+        icameg.save("{dir}nc_{sub}_{run}_hand_meg-ica.fif".format(dir=proc_dir,
+                                                                  sub=sub,
+                                                                  run=run))
